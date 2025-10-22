479770a96628809409a8dc5ace0ea58fb9e54235
Bhairavi <bhandaririddhima8@gmail.com>
2025-10-22 14:53:03 +0000

    Modified Javascript file

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
 
+notenames = new Map();
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440.0);
+notenames.set("B", 493.9);
+
 function frequency(pitch){
     gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
@@ -24,5 +33,6 @@ audioCtx.resume();
 gainNode.gain.value = 0;
 
 function handle(){
-    frequency(input.value);
+    var usernotes = String(input.value);
+    frequency(notenames.get(usernotes));
 }
